56a16db273ffb74fce7a27737db72a2a941b6fd8
Schwenk-A <100094451+Schwenk-A@users.noreply.github.com>
2022-08-30 09:17:12 -0500

    Created Template

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticKeywordExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
